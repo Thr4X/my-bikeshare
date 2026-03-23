2fba8385a1d4bb53bb3942cf76a4f3e7317fb577
fixcmt1 <thorben.meier@cariad.technology>
2026-03-23 14:40:02 +0100

    Added type annotations for 'time_stats' and 'station_stats'

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -45,7 +45,7 @@ def load_data(city: str, month: str, day: str) -> pd.DataFrame:
     return df
 
 
-def time_stats(df):
+def time_stats(df: pd.DataFrame):
     """Displays statistics on the most frequent times of travel."""
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
@@ -64,7 +64,7 @@ def time_stats(df):
     print('-'*40)
 
 
-def station_stats(df):
+def station_stats(df: pd.DataFrame):
     """Displays statistics on the most popular stations and trip."""
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
@@ -83,7 +83,7 @@ def station_stats(df):
     print('-'*40)
 
 
-def trip_duration_stats(df):
+def trip_duration_stats(df: pd.DataFrame):
     """Displays statistics on the total and average trip duration."""
 
     print('\nCalculating Trip Duration...\n')
@@ -99,7 +99,7 @@ def trip_duration_stats(df):
     print('-'*40)
 
 
-def user_stats(df):
+def user_stats(df: pd.DataFrame):
     """Displays statistics on bikeshare users."""
 
     print('\nCalculating User Stats...\n')
